26f0dff99fc7188f770ace435cfe819713f6caea
DrewWham <fcw5014@psu.edu>
2019-03-24 13:06:02 -0400

    update code to data.table

--- plotSTR.r
+++ plotSTR.r
@@ -1,5 +1,6 @@
 library(stringr)
 library(ggplot2)
+library(data.table)
 
 #function for extracting the cluster Probs, requires STR infile because STRUCTURE likes to chop off the ends of your sample names so I have to use your original file to get your original names
 read.STR<-function(STR.in,STR.out){
@@ -27,25 +28,26 @@ write.csv(qs,paste("k",nclust,".csv",sep=""),row.names=F)
 
 
 #this function plots the structure plot, it takes the result of read.STR, it will also optionaly take a file to reorder the plot, if you dont already have one just reorder your outfile from above and save it
-plotSTR<-function(Cluster.csv,order=Cluster.csv){
+plotSTR<-function(Cluster.csv,plt.order=Cluster.csv){
 #I turn off warnings because it will tell you about duplicated factors below 
 oldw <- getOption("warn")
 options(warn = -1)
 #read data
-data<-read.csv(Cluster.csv)
-order<-read.csv(order)
+data<-fread(Cluster.csv)
+plt.order<-fread(plt.order)
 nclust<-dim(data)[2]-1
 #relabel the columns
 labs<-c("Sample",paste("Cluster",seq(1:nclust),sep=""))
-names(data)<-labs
+og.labs<-paste("X",seq(1:(nclust+1)),sep="")
+setnames(data,og.labs,labs)
 #reorder, this doesnt do anything if you dont supply an order file
-data<-data[match(order[,1],data$Sample),]
+data<-data[match(plt.order$X1,data$Sample),]
 #remove duplicates, you shouldnt have duplicates anyway
 data<-data[!duplicated(data$Sample),]
 #I melt here to make the plotting easy
 mdata<-melt(data)
 names(mdata)<-c("Sample","Species","Probability")
-mdata$Sample<-factor(mdata$Sample,levels=mdata$Sample)
+mdata$Sample<-factor(mdata$Sample,levels=data$Sample)
 #plot, you can change the palette to something else to explore other color themes
 ggplot(mdata,aes(x=Sample,y=Probability,fill=Species))+geom_bar(stat="identity",position="stack")+theme_classic()+ scale_fill_brewer(palette="Spectral")+theme(axis.text.x = element_text(size=5, angle = 90, hjust = 1,colour="black"))
 ggsave(paste("k",nclust,"STRplot.pdf",sep=""),width = 20, height = 9)
